b96e2c665529b80dcbc6f34d44c25e42a08227cd
kalpna-hub <122824448+kalpna-hub@users.noreply.github.com>
2023-05-01 19:05:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+// Get all the prices from the table
+  const prices = document.querySelectorAll('[data-ns-test="prices"]');
+  
+  // Initialize the total price to zero
+  let totalPrice = 0;
+  
+  // Loop through each price and add it to the total price
+  prices.forEach(price => {
+    totalPrice += parseFloat(price.innerText);
+  });
+  
+  // Set the total price in the last row of the table
+  const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
+  grandTotal.innerText = totalPrice.toFixed(2);
